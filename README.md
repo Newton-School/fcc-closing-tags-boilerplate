f8897cef8813cf263fc52bc9181d7ee227159549
Akshat Sethi <akshat@nova-benefits.com>
2022-05-30 00:02:46 +0530

    done

---
+++ README.md
@@ -0,0 +1,4 @@
+# React JSX tags
+
+The code provided in this assignment does not work, Identify the errors in 
+the JSX code in <code>src/index.js</code> and correctly render the output.
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const JSX = (
+    <div>
+      <h2>Welcome to React!</h2> <br>
+      <p>Be sure to close all tags!</p>
+      <hr>
+    </div>
+);
+
+
+ReactDOM.render(JSX, document.getElementById("root"));
